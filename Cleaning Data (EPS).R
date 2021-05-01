19ed2d6147958b7593d5ef417f6c3bee90f28a43
robemar <66203393+robemar@users.noreply.github.com>
2021-05-01 17:03:21 +0200

    Cleaning of Data

---
+++ Cleaning Data (EPS).R
@@ -0,0 +1,24 @@
+
+rm(list = ls())
+setwd("D:/Master Denmark/3rd Semester/Political Data Science/Final Exam/True Final Exam/1. Downloading")
+
+library(dplyr)
+
+
+#load parsing results
+load("D:/Master Denmark/3rd semester/Political Data Science/Final Exam/True Final Exam/1. Downloading/ElPaisSanchezText.RData")
+
+#clean parsing results
+
+
+#repeated articles (with different titles)
+textclean <- finaldata %>% distinct(full_text, .keep_all = TRUE)
+
+#articles with errors, we write "a" as it is one of the most common letters in words 
+find.hits <- grepl("a", textclean$full_text, ignore.case = T)
+
+fulltexttext <- textclean$full_text[find.hits]
+
+save(fulltexttext, file = "Sanchez(ElPais).RData")  #just in case
+save(textclean, file = "Sanchez(ElPais)clean.RData")
+
